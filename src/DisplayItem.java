3f62c9a3010dccc06c0dabb1525d4e7edd06b612
IFeel3 <wincentyzkielczy1337@gmail.com>
2017-03-16 10:17:24 +0100

    deleting records

--- src/DisplayItem.java
+++ src/DisplayItem.java
@@ -4,26 +4,55 @@ import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
 import javax.microedition.lcdui.TextBox;
+import javax.microedition.lcdui.TextField;
+import javax.microedition.rms.RecordStore;
 
 public class DisplayItem extends TextBox implements CommandListener{
 
 	Command backCommand;
+	Command deleteRecordCommand;
 	Displayable mainScreen;
 	Display display;
 	TextBox textBox;
+	TextBox indexBox;
+	Command saveCommand;
 	
 	public DisplayItem(String title, Displayable mainScreen, Display display) {
 		super("Display Item", "", 256, 0);
 		this.mainScreen = mainScreen;
 		this.display = display;
 		backCommand = new Command("Back", Command.EXIT, 1);
+		deleteRecordCommand = new Command("delete record", Command.ITEM, 1);
 		addCommand(backCommand);
+		addCommand(deleteRecordCommand);
 		setCommandListener((CommandListener)this);
+		indexBox = new TextBox("index", "", 2, TextField.NUMERIC);
+		saveCommand = new Command("save", Command.ITEM, 1);
+		indexBox.addCommand(saveCommand);
+		indexBox.setCommandListener(new DeleteCommandListener());
 	}
 
 	public void commandAction(Command c, Displayable d) {
 		if (c == backCommand) {
 			display.setCurrent(mainScreen);
+		}
+		if (c == deleteRecordCommand) {
+			display.setCurrent(indexBox);
+		}
+	}
+	
+	private class DeleteCommandListener implements CommandListener {
+		public void commandAction(Command c, Displayable d) {
+			if (c == saveCommand) {
+				RecordStore rs = MyMidlet.recordStore;
+				int index = Integer.parseInt(indexBox.getString());
+				try {
+					rs.deleteRecord(index);
+				} catch(Exception e) {
+					// handle
+				}
+				display.setCurrent(mainScreen);
+			}
 		}
 	}
 
